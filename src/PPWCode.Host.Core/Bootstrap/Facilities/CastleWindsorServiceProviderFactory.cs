1a5f12f69ff99b6799cfe064d6d9ca5f4060329b
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2020-07-13 17:28:46 +0200

    Guard against multiple CreateBuilder / CreateServiceProvider calls

--- src/PPWCode.Host.Core/Bootstrap/Facilities/CastleWindsorServiceProviderFactory.cs
+++ src/PPWCode.Host.Core/Bootstrap/Facilities/CastleWindsorServiceProviderFactory.cs
@@ -26,6 +26,10 @@ namespace PPWCode.Host.Core.Bootstrap.Facilities
     public class CastleWindsorServiceProviderFactory
         : IServiceProviderFactory<IContainerBuilder>
     {
+        private readonly object _locker = new object();
+        private volatile PPWContainerBuilder _containerBuilder;
+        private volatile ServiceProvider _serviceProvider;
+
         public CastleWindsorServiceProviderFactory(
             [NotNull] ContainerWrapper containerWrapper,
             [NotNull] string dllPrefix,
@@ -57,38 +61,58 @@ namespace PPWCode.Host.Core.Bootstrap.Facilities
 
         public IContainerBuilder CreateBuilder([NotNull] IServiceCollection services)
         {
-            // install custom controller-activator + request-scoping-middleware
-            services.AddWindsorServices(ContainerWrapper.WindsorContainer);
-
-            // Setup the container
-            ContainerWrapper
-                .Initialise(
-                    DllPrefix,
-                    container =>
+            if (_containerBuilder == null)
+            {
+                lock (_locker)
+                {
+                    if (_containerBuilder == null)
                     {
-                        container
-                            .AddSubResolverConditionally(c => new PPWCodeCollectionResolver(c.Kernel, true))
-                            .AddFacilityConditionally<AspNetCoreFacility>(f => f.CrossWiresInto(services));
-                        OnSetupContainer?.Invoke(container);
-                    },
-                    OnBeforeInstall,
-                    OnAfterInstall);
+                        // install custom controller-activator + request-scoping-middleware
+                        services.AddWindsorServices(ContainerWrapper.WindsorContainer);
 
-            return new PPWContainerBuilder(ContainerWrapper, services);
+                        // Setup the container
+                        ContainerWrapper
+                            .Initialise(
+                                DllPrefix,
+                                container =>
+                                {
+                                    container
+                                        .AddSubResolverConditionally(c => new PPWCodeCollectionResolver(c.Kernel, true))
+                                        .AddFacilityConditionally<AspNetCoreFacility>(f => f.CrossWiresInto(services));
+                                    OnSetupContainer?.Invoke(container);
+                                },
+                                OnBeforeInstall,
+                                OnAfterInstall);
+
+                        _containerBuilder = new PPWContainerBuilder(ContainerWrapper, services);
+                    }
+                }
+            }
+
+            return _containerBuilder;
         }
 
         [NotNull]
         public IServiceProvider CreateServiceProvider(IContainerBuilder containerBuilder)
         {
-            // ensure that all registrations in IServiceCollection are done,
-            // before building the service-provider!
-            ServiceProvider serviceProvider = containerBuilder.ServiceCollection.BuildServiceProvider();
+            if (_serviceProvider == null)
+            {
+                lock (_locker)
+                {
+                    if (_serviceProvider == null)
+                    {
+                        // ensure that all registrations in IServiceCollection are done,
+                        // before building the service-provider!
+                        _serviceProvider = containerBuilder.ServiceCollection.BuildServiceProvider();
 
-            // finalize castle windsor integration
-            // important for cross-wiring: passing the IServiceProvider
-            containerBuilder.ServiceCollection.AddWindsorIntegration(containerBuilder.Container.WindsorContainer, () => serviceProvider);
+                        // finalize castle windsor integration
+                        // important for cross-wiring: passing the IServiceProvider
+                        containerBuilder.ServiceCollection.AddWindsorIntegration(containerBuilder.Container.WindsorContainer, () => _serviceProvider);
+                    }
+                }
+            }
 
-            return serviceProvider;
+            return _serviceProvider;
         }
     }
 }
